6c9d0e1f9ecbe4467592770b2386e815a3803bad
Andrey Gribanov <shade1471@gmail.com>
2022-02-11 22:56:29 +0700

    ADD Junit4 and junit jupiter5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
     private CashbackHackService service = new CashbackHackService();
 
+    //Junit4
     @Test
     public void shouldCalculate() {
 
@@ -38,5 +39,39 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1000);
 
         assertEquals(0, actual);
+    }
+
+    //Junit5
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateJU5() {
+
+        int actual = service.remain(900);
+
+        org.junit.jupiter.api.Assertions.assertEquals(100, actual);
+    }
+
+    @Test
+    public void shouldCalculateMinRemainJU5() {
+
+        int actual = service.remain(1999);
+
+        org.junit.jupiter.api.Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    public void shouldCalculateMaxRemainJU5() {
+
+        int actual = service.remain(2001);
+
+        org.junit.jupiter.api.Assertions.assertEquals(999, actual);
+    }
+
+    @Test
+    public void shouldCalculateBoundaryValuesJU5() {
+
+        int actual = service.remain(1000);
+
+        org.junit.jupiter.api.Assertions.assertEquals(0, actual);
     }
 }
